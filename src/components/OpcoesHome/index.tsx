adc424ab09732b644d35650790de3a07bb96695d
Felipe Santos <felipesantos_fcs98@hotmail.com>
2023-09-13 15:44:23 -0300

    Corrigindo bugs para produção

--- src/components/OpcoesHome/index.tsx
+++ src/components/OpcoesHome/index.tsx
@@ -39,7 +39,7 @@ export default function OpcoesHome({ children, src, alt, path, itens }: Props) {
   const router = useRouter()
   return (
     <List >
-      <ListItemButton onClick={handleClick} sx={{background: 'whitesmoke'}} className={styles.listaContainer}>
+      <ListItemButton onClick={handleClick} sx={{background: 'whitesmoke', borderRadius: '10px'}} className={styles.listaContainer}>
         <ListItemIcon>
           <Image
             src={src}
@@ -57,7 +57,7 @@ export default function OpcoesHome({ children, src, alt, path, itens }: Props) {
             if (item.titulo !== '') {
               return (
                 <>
-                  <ListItemButton className={styles.listaItem} sx={{background: 'aliceblue'}} onClick={() => router.push(item.pathItem)}>
+                  <ListItemButton className={styles.listaItem} sx={{background: 'aliceblue', borderRadius: '10px'}} onClick={() => router.push(item.pathItem)}>
                     <ListItemIcon>
                       <StarBorder />
                     </ListItemIcon>
